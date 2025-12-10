4fabfd8f59b4b67e470f256a501817eb7f731a56
sakthiswaran2705 <sakthibala2705@gmail.com>
2025-12-10 20:07:25 +0530

    Update searched_detail_showhome.py

--- api/searched_detail_showhome.py
+++ api/searched_detail_showhome.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from bson import ObjectId
-from common_urldb import db
+from api.common_db_url import db
 from datetime import datetime
 router = APIRouter()
 col_category = db["category"]
