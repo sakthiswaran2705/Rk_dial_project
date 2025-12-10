b05e63f4cf46e092e8f9720ecb5bb6975bf9605f
sakthiswaran2705 <sakthibala2705@gmail.com>
2025-12-10 19:52:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# Import Routers
-from search_shops import router as search_router
-from shop_owner_details import router as owner_router
-from searched_detail_showhome import router as searched_detail_router
-from offers_city import router as slideshow_router
-from offers_list import router as offer_router
+# Import Routers (corrected paths)
+from api.search_shops import router as search_router
+from api.shop_owner_details import router as owner_router
+from api.searched_detail_showhome import router as searched_detail_router
+from api.offers_city import router as slideshow_router
+from api.offers_list import router as offer_router
+
 app = FastAPI(
     title="RK-DIAL API",
     description="API endpoints for RK-Dial Application",
@@ -14,6 +15,7 @@ app = FastAPI(
     docs_url="/api/",
     redoc_url=None
 )
+
 # CORS
 app.add_middleware(
     CORSMiddleware,
@@ -22,12 +24,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Include Routers
+# Router include
 app.include_router(search_router)
 app.include_router(owner_router)
 app.include_router(searched_detail_router)
 app.include_router(slideshow_router)
 app.include_router(offer_router)
+
 # Root
 @app.get("/")
 def root():
