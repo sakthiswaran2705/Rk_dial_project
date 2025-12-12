fda0187b69901a34bd5c423d17d497890a0fdc93
sakthiswaran2705 <sakthibala2705@gmail.com>
2025-12-12 19:46:40 +0530

    Add files via upload

---
+++ api/offers_for_every_shop.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter
+from bson import ObjectId
+from common_urldb import db
+
+router = APIRouter()
+
+col_offers = db["offers"]   # { shop_id: "id", offers: [...] }
+
+@router.get("/offers/shop/{shop_id}/")
+def get_offers_for_shop(shop_id: str):
+
+    try:
+        oid = ObjectId(shop_id)
+    except:
+        oid = None
+
+    # Match both ObjectId or string
+    offers_doc = col_offers.find_one({
+        "$or": [
+            {"shop_id": shop_id},
+            {"shop_id": oid}
+        ]
+    })
+
+    # No offers for this shop
+    if not offers_doc:
+        return {"status": True, "offers": []}
+
+    offers_list = []
+
+    for offer in offers_doc.get("offers", []):
+        offers_list.append({
+            "offer_id": offer.get("offer_id"),
+            "title": offer.get("title"),
+            "percentage": offer.get("percentage"),
+            "file_type": offer.get("file_type"),
+            "file_base64": offer.get("file_base64"),
+            "filename": offer.get("filename"),
+            "description": offer.get("description"),
+            "start_date": offer.get("start_date"),
+            "end_date": offer.get("end_date"),
+            "fee": offer.get("fee")
+
+        })
+
+    return {
+        "status": True,
+        "offers": offers_list
+    }
