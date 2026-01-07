315827b36e3b9ee3471d934cbb2edf12982709e4
sakthiswaran2705 <sakthibala2705@gmail.com>
2026-01-07 18:40:38 +0530

    Update JobDetails.jsx

--- frontend/src/JobDetails.jsx
+++ frontend/src/JobDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
 
 export default function JobDetails() {
   const { id } = useParams();
