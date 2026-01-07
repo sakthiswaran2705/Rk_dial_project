75238332df2fa00f315749dc7562e8a90e92a73c
sakthiswaran2705 <sakthibala2705@gmail.com>
2026-01-07 18:58:22 +0530

    Update login.jsx

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 // ----------------- Icons -----------------
 const Eye = (
