944239f3bc3af370a71e96acac96b21203a1c724
sakthiswaran2705 <sakthibala2705@gmail.com>
2026-01-07 18:46:27 +0530

    Update Jobs.jsx

--- frontend/src/Jobs.jsx
+++ frontend/src/Jobs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
 
 export default function Jobs() {
   const navigate = useNavigate();
