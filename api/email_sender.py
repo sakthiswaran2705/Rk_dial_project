a71d48f705c5a87afcdeebb6b80f17ec0a9099a7
sakthiswaran2705 <sakthibala2705@gmail.com>
2026-01-09 13:01:08 +0530

    Update email_sender.py

--- api/email_sender.py
+++ api/email_sender.py
@@ -4,16 +4,16 @@ from email.mime.text import MIMEText
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USER = "sakthibala2705@gmail.com"
-EMAIL_PASS = "mrbg njvo xafw pwfd"
+EMAIL_PASS = "APP_PASSWORD"  # DO NOT hardcode
 
 def send_email(to_email, subject, body):
     msg = MIMEText(body, "html")
     msg["Subject"] = subject
     msg["From"] = EMAIL_USER
     msg["To"] = to_email
-    server.set_debuglevel(1)
 
     server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+    server.set_debuglevel(1)  # AFTER server creation
     server.starttls()
     server.login(EMAIL_USER, EMAIL_PASS)
     server.send_message(msg)
